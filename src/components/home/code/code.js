093cc5ab0f617e3966766a7450cf1e6db8a6c985
Eric Smith <heathornhawk@gmail.com>
2017-03-15 19:16:28 -0700

    updates

--- src/components/home/code/code.js
+++ src/components/home/code/code.js
@@ -41,6 +41,36 @@ export default class Code extends Component {
                             ids={['angular', 'npm','grunt', 'html', 'css', 'javascript', 'sass', 'bower', 'sketch', 'git']} />
                     </div>
 
+                    <div className={styles.item}>
+                        <h4>Ghost - WPF</h4>
+                        <p>
+                            This was an application I built because I hated redlining.
+                        </p>
+                        <div className={styles.linkContainer}>
+                            <a target="blank" href={require('downloads/Ghost.zip')}>Download Application (Windows Only)</a>
+                        </div>
+                        <Skills
+                            legend={false}
+                            ategory='all'
+                            size='small'
+                            ids={['wpf', 'csharp','xaml', 'photoshop', 'windows']} />
+                    </div>
+
+                    <div className={styles.item}>
+                        <h4>HVCC - WPF Prototype</h4>
+                        <p>
+                            This was my attempt at extending Ghost in a way that could be used with a prototype. Developers installed this application and used it to guide them through the UI. They also used it to verify their work to make sure it matched the design EXACTLY! 
+                        </p>
+                        <div className={styles.linkContainer}>
+                            <a target="blank" href={require('downloads/HVCC.zip')}>Download Application (Windows Only)</a>
+                        </div>
+                        <Skills
+                            legend={false}
+                            ategory='all'
+                            size='small'
+                            ids={['wpf', 'csharp','xaml', 'photoshop', 'windows']} />
+                    </div>
+
                     <br/><br/><br/><br/><br/><br/><br/><br/>
                 </div>
             </div>
